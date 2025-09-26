4d2f1bddf76e0267bffcf4243e520926b341faf6
Onyedikachi Oji <ocoji@myseneca.ca>
2025-09-26 07:39:43 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3 
+#Author: Oji Onyedikachi Christopher
+#Author ID: 133383224
+#Date Created: 2025/09/25
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
