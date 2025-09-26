ae245cec3f96316d171b9e76ce3e5a4640226683
Onyedikachi Oji <ocoji@myseneca.ca>
2025-09-26 07:39:14 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+#Author; Oji Onyedikachi Christopher
+#Author ID: 133383224
+#Date Created: 2025/09/25
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
